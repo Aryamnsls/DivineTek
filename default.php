75a90707659463352f9ee0b5147fa5fdb3d13c3f
Aryaman Singha <aryamansingha60@gmail.com>
2026-06-29 21:37:05 +0530

    Remove temporary PHP override files

--- default.php
+++
@@ -1,4 +0,0 @@
-<?php
-// This file overrides Hostinger's default page and serves the React app
-include __DIR__ . '/dist/index.html';
-?>
--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// This index.php is created to take priority over Hostinger's default.php
-include __DIR__ . '/dist/index.html';
-?>
